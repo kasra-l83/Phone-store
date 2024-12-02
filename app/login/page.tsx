4e1b379e34713539a3f1b131f149aed1fecddb8d
kasra-l83 <o.kasralatifi@gmail.com>
2024-12-03 00:46:45 +0330

    factoring-2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,11 +5,7 @@ const LoginPage = () =>{
   return (
     <section className='max-w-[440px] h-[479px] mx-auto border border-blue-500 rounded-lg px-8 flex flex-col items-center justify-around'>
       <div className="flex items-center gap-x-2">
-<<<<<<< HEAD
-        <Image src="/icon/logo.png" alt="Logo" width={72} height={72}/>
-=======
         <Image src="/icon/logo.png" alt='Logo' width={72} height={72}/>
->>>>>>> develop
         <h2 className="text-2xl text-blue-900 font-semibold">PLAY<span className="text-yellow-700">MEDA</span></h2>
       </div>
       <h4 className='text-2xl font-bold'>ورود ادمین</h4>
