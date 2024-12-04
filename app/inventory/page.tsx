16ff19456699255f12615c7dc73c0cdd1c3c6df1
kasra-l83 <o.kasralatifi@gmail.com>
2024-12-05 00:20:01 +0330

    fix inventory pagination

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 
 export default function Orders() {
     const [products, setProducts] = useState([]);
+    const [page, setPage]= useState<number>(1);
+    const [pages, setPages]= useState<number>(0);
     const session= localStorage.getItem("token")
     const { push } = useRouter();
     if(!session){
@@ -11,17 +13,34 @@ export default function Orders() {
     }
     useEffect(() => {
         const fetchOrders = async () => {
-            const response = await fetch('http://localhost:8000/api/products');
+            const response = await fetch(`http://localhost:8000/api/products?page=${page}&limit=3`);
             const data = await response.json();
             setProducts(data.data.products);
-            console.log(data.data.products);
+            setPages(data.total_pages)
+            console.log(data);
         }
 
         fetchOrders();
-    }, []);
+    }, [page]);
+    const next= () =>{
+        if(page< pages){
+            setPage(page + 1)
+        }
+    }
+    const before= () =>{
+        if(page> 1){
+            setPage(page - 1)
+        }
+    }
     return (
         <>
-            <h2 className="sm:text-3xl text-2xl font-semibold mb-5">مدیریت موجودی و قیمت ها</h2>
+            <div className="mb-5 flex justify-between">
+                <h2 className="sm:text-3xl text-base font-semibold">مدیریت موجودی و قیمت ها</h2>
+                <span className="flex gap-x-2">
+                    <button onClick={() => before()} className={`${page===1 ? "hidden" : ""} bg-blue-500 hover:bg-blue-700 text-white px-2 py-1 rounded`}>قبلی</button>
+                    <button onClick={() => next()} className={`${page===pages ? "hidden" : ""} bg-blue-500 hover:bg-blue-700 text-white px-2 py-1 rounded`}>بعدی</button>
+                </span>
+            </div>
             <table className="border-2 border-black max-w-[1000px] w-full mx-auto text-right">
                 <thead className="bg-gray-400 text-white">
                     <tr>
