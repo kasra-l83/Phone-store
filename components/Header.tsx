880bc7119bbcf97e53a78f7f1d17125e4d1f80d3
kasra-l83 <o.kasralatifi@gmail.com>
2024-12-26 10:47:00 +0330

    fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,7 @@ import { FaPencilRuler } from "react-icons/fa";
 import { FaPhoneFlip } from "react-icons/fa6";
 import { useAppSelector } from "../redux/hook";
 import { ITodo } from "@/types/todo";
+import { Auth } from "@/providers/auth.provider";
 
 export default function Header() {
     const todoList= useAppSelector((state) => state.cart);
@@ -20,16 +21,21 @@ export default function Header() {
     const { push }= useRouter();
 
     const session= localStorage.getItem("token");
+    const {user, logout}= Auth();
     
     const click= () =>{
         push("/login")
     }
     const click1= () =>{
-        push("/cart");
         setOpen1(false);
+        push("/cart");
     }
     const click2= () =>{
         setOpen1(true)
+    }
+    const Logout= () =>{
+        localStorage.removeItem("token");
+        logout();
     }
     const openHandler= () => setOpen(true);
     const closeHandler= () => setOpen(false);
@@ -37,17 +43,17 @@ export default function Header() {
         <>
         <header className="flex flex-col justify-center gap-y-3 p-5">
             <div className="flex flex-row-reverse justify-between items-center sm:flex-row">
-                <span className={`gap-x-10 text-base text-gray-400 font-normal ${!session? "hidden" : "hidden sm:flex"}`}>
+                <span className={`gap-x-10 text-base text-gray-400 font-normal ${user !== "admin"? "hidden" : "hidden sm:flex"}`}>
                     <a href="/admin/orders" className="hover:text-blue-500">سفارشات</a>
                     <a href="/admin/inventory" className="hover:text-blue-500">موجودی</a>
                     <a href="/admin/products" className="hover:text-blue-500">محصولات</a>
-                    <a onClick={() => localStorage.removeItem("token")} href="/" className="hover:text-blue-500">خروج</a>
+                    <a onClick={Logout} href="/" className="hover:text-blue-500">خروج</a>
                 </span>
-                <span className="flex items-center gap-x-2">
+                <span className={`flex items-center gap-x-2 ${user==="admin" ? "flex-row-reverse" : ""}`}>
                     <Image src="/icon/logo.png" alt='Logo' width={72} height={72}/>
                     <h1 className="text-2xl text-blue-900 font-semibold hidden sm:block">PLAY<span className="text-yellow-700">MEDA</span></h1>
                 </span>
-                <span className={`hidden gap-x-5 xl:gap-x-10 text-base text-gray-400 font-normal ${!session? "lg:flex" : ""}`}>
+                <span className={`hidden gap-x-5 xl:gap-x-10 text-base text-gray-400 font-normal ${user !== "admin"? "lg:flex" : ""}`}>
                     <a href="/" className="hover:text-blue-500">صفحه اصلی</a>
                     <a href="/products" className="hover:text-blue-500">محصولات</a>
                     <a href="/category" className="hover:text-blue-500">دسته بندی</a>
@@ -55,8 +61,8 @@ export default function Header() {
                     <a href="/contact" className="hover:text-blue-500">تماس با ما</a>
                     <a href="/about" className="hover:text-blue-500">درباره ما</a>
                 </span>
-                <span className={`flex gap-x-5 ${!session ? "" : "hidden"}`}>
-                    <button onClick={click} className="text-blue-500 w-40 py-[10px] bg-gray-100 rounded-lg hover:bg-gray-300 hidden sm:block">ورود یا ثبت نام</button>
+                <span className={`flex gap-x-5 ${user=== "admin" ? "hidden" : ""}`}>
+                    <button onClick={click} className={`text-blue-500 w-40 py-[10px] bg-gray-100 rounded-lg hover:bg-gray-300 ${session? "hidden" : "hidden sm:block"}`}>ورود یا ثبت نام</button>
                     <button disabled={todoList.list.length<= 0} onClick={click2} className="text-xl text-blue-500 p-[10px] bg-gray-100 rounded-lg relative">
                         <IoCartOutline/>
                         <div className={`bg-blue-500 text-white absolute right-[-10px] top-[-10px] rounded-full size-6 justify-center items-center ${todoList.list.length> 0 ? "flex" : "hidden"}`}>{todoList.list.length}</div>
